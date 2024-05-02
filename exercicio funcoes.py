2fdd43c5a6c434b5671495d152ea6d45cbbdd9a0
Aguinaldo Anselmo Da Costa Neto <13173399404@UPE.IO>
2024-05-02 14:39:34 -0300

    Adicionei as questoes 3, 4 e 5

--- exercicio funcoes.py
+++ exercicio funcoes.py
@@ -3,17 +3,17 @@
 def borda():
     print("+ - - - - + - - - - +")
 
-
 def linhas():
     for x in range(4):
         print("|         |         |")
 
 def montar():
-    borda()
-    linhas()
-    borda()
-    linhas()
-    borda()
+    
+   borda()
+   linhas()
+   borda()
+   linhas()
+   borda()
 
 montar()
 
@@ -75,4 +75,67 @@ print("FIM DA SEGUNDA QUESTÃO AUTOMATIZADA")
 
 #3. Questão
 
+inputNumero1 = float(input("Bem vindo a calculadora do Neto, favor inserir seu primeiro número "))
+inputNumero2 = float(input("Agora o segundo número, princesx: "))
+
+def adicao(num1, num2):
+    return(num1 + num2)
+
+def subtracao(num1, num2):
+    return(num1 - num2)
+
+def divisao(num1, num2):
+    return(num1 / num2)
+def multiplicacao(num1, num2):
+    return(num1 * num2)
+def exponenciacao(num1, num2):
+    return(num1 ** num2)
+
+print("ADIÇÃO:", adicao(inputNumero1, inputNumero2), "SUBTRAÇÃO:", subtracao(inputNumero1, inputNumero2), "DIVISÃO:", divisao(inputNumero1, inputNumero2), "MULTIPLICAÇÃO:", multiplicacao(inputNumero1, inputNumero2), "EXPONENCIAÇÃO:", exponenciacao(inputNumero1, inputNumero2))
+print("FIM DA TERCEIRA QUESTAO")
+
+#4. Questão
+
+inputCoordenadas = input("Insira as Coordenadas: ").split(" ")
+xUser = int(inputCoordenadas[0])
+yUser = int(inputCoordenadas[1])
+
+def distanciaPontos(X, Y):
+    print((X**2 + Y**2) ** 0.5)
+
+distanciaPontos(xUser, yUser)
+
+print("FIM DA QUARTA QUESTÃo")
+
+#5. Questão
+
+saldo = 0.0
+
+print("Empresa VEM onibus de neto")
+
+print("INSIRA O VALOR DA SUA PRIMEIRA RECARGA")
+
+def mostrarSaldo():
+    global saldo
+    print("Seu saldo atual é {:.2f}".format(saldo))
+
+def adicionarSaldo(valorAdicionado):
+    global saldo
+    saldo += valorAdicionado
+    mostrarSaldo()
+
+def usarCartao():
+    global saldo
+    saldo -= 4.10
+    mostrarSaldo()
+
+adicionarSaldo(float(input()))
+usarCartao()
+
+print("FIM DA QUINTA QUESTÃO")
+
+
+
+
+    
 
