e115c3112cfe3310e6fad981d1f7c562217e9883
netokemon <aguinaldoanselmo5@gmail.com>
2024-05-03 12:43:13 -0300

    Fix n.4

--- 4 - Distancia entre dois pontos.py
+++ 4 - Distancia entre dois pontos.py
@@ -1,8 +1,10 @@
-inputCoordenadas = input("Insira as Coordenadas X e Y como: X,Y\n ").split(",")
-xUser = int(inputCoordenadas[0])
-yUser = int(inputCoordenadas[1])
+inputCoordenadas = input("Insira as Coordenadas X1,Y1,X2,Y2\n ").split(",")
+x1User = int(inputCoordenadas[0])
+y1User = int(inputCoordenadas[1])
+x2User = int(inputCoordenadas[2])
+y2User = int(inputCoordenadas[3])
 
-def distanciaPontos(X, Y):
-    print((X**2 + Y**2) ** 0.5)
+def distanciaPontos(X1, Y1, X2, Y2):
+    print(((X2 - X1)**2 + (Y2-Y1)**2)** 0.5)
 
-distanciaPontos(xUser, yUser)
+distanciaPontos(x1User, y1User, x2User, y2User)
